465e3cd13e667f6b4489fe99885bfa2c99b7103e
ryuta0419 <chupp.ppuhc@gmail.com>
2017-01-11 16:56:02 +0900

    プログラミング基礎知識２、switch文2提出します。

---
+++ CampChallenge_PHP_解凍/002.プログラミング基礎学習２/switch文2.php
@@ -0,0 +1,15 @@
+<?php
+
+$nom = 'A';
+$message = '';
+ switch($nom){
+     case 'A':
+       $message = '英語';
+       break;
+
+     case 'あ':
+       $message = '日本語';
+       break;
+}
+
+  echo $message;
